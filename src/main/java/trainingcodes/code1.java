0e70e569db510d9ddf4b0f4afc7212e2bee6fadc
hp <hp@DESKTOP-7M3N3CA>
2023-04-11 02:41:45 -0700

    pattern code1

---
+++ src/main/java/trainingcodes/code1.java
@@ -0,0 +1,24 @@
+package trainingcodes;
+import java.util.Scanner;
+public class code1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value of n:");
+		int n=sc.nextInt();
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=n; j++) {
+			if(i==1 || i==n || j==1 || j==n) {
+				System.out.print("$");
+			}
+			else {
+				System.out.print("-");
+			}
+		}
+			System.out.println();
+}
+
+	}
+
+}

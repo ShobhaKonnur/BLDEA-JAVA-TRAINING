fb32e24c4ed504f009047feaa632610de06ee16b
hp <hp@DESKTOP-7M3N3CA>
2023-04-11 02:59:33 -0700

    codeNumber1 added

---
+++ src/main/java/trainingcodes/codeNumber1.java
@@ -0,0 +1,14 @@
+package trainingcodes;
+
+public class codeNumber1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int count=25;
+		for(int i=1; i<=5; i++) {
+			System.out.print(count+"  ");
+			count=count-5;
+		}
+	}
+
+}
